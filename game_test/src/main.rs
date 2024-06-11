0f064702ad676852918abf8c61bfab7ab384588b
Min <minthuh@student.unimelb.edu.au>
2024-06-11 22:44:53 +1000

    added correct path for arial10x10.png

--- game_test/src/main.rs
+++ game_test/src/main.rs
@@ -931,7 +931,7 @@ fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
     let root = Root::initializer()
-        .font("arial10x10.png", FontLayout::Tcod)
+        .font("images/arial10x10.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Rust/libtcod tutorial")
